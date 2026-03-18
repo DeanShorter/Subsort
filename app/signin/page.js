f3c183216f243e9b8baa906b489a667f5cb52c3c
Dean <deanage95@googlemail.com>
2026-03-18 23:06:10 +0000

    bug

--- app/signin/page.js
+++ app/signin/page.js
@@ -7,9 +7,7 @@ const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 
 export default function SignIn() {
   useEffect(() => {
-    const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-      auth: { flowType: 'implicit' },
-    });
+    const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
     sb.auth.signInWithOAuth({
       provider: 'google',
       options: {
